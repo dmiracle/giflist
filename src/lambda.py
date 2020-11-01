9b15aeb802a28b04b6aa6792e0cbefac3fbb7c42
Dylan Miracle <dylan.miracle@gmail.com>
2020-11-01 13:58:58 -0600

    add ignore

---
+++ src/lambda.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+
+params = {
+     'region' : 'us-east-1',
+     'database' : 'giflistdb',
+     'bucket' : 'gif-list-database-3b8cb02a',
+     'path'  : 'queries',
+     'query': 'SELECT * FROM "giflistdb"."users" limit 30;'
+     }
+
+def lambda_handler(event, context):
+    client = boto3.client('athena')
+    response_query_execution_id = client.start_query_execution(
+        QueryString = params['query'],
+        QueryExecutionContext = {
+            'Database' : params['database']
+        },
+        ResultConfiguration = {
+            'OutputLocation': 's3://' + params['bucket'] + '/' + params['path']
+        }
+    )
+    response_get_query_details = client.get_query_execution(
+        QueryExecutionId = response_query_execution_id['QueryExecutionId']
+    )
+
+    print(response_get_query_details)
+    return {
+        'statusCode': 200,
+        'response': str(response_get_query_details)
+    }
